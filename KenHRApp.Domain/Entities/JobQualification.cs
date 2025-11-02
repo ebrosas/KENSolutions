8dcefc5b9601139c29ff919e808b9c531780b4c8
ebrosas <ervin.brosas@yahoo.com>
2025-11-02 23:28:24 +0300

    Added RecruitmentRequest and JobQualification entities

---
+++ KenHRApp.Domain/Entities/JobQualification.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KenHRApp.Domain.Entities
+{
+    public class JobQualification
+    {
+        #region Properties
+        public int AutoId { get; set; }
+        public string QualificationCode { get; set; } = null!;
+        public string Qualification { get; set; } = null!;
+        public string StreamCode { get; set; } = null!;
+        public string Stream { get; set; } = null!;
+        public string? SpecializationCode { get; set; } = null;
+        public string? Specialization { get; set; } = null;
+
+        [Column(TypeName = "varchar(500)")]
+        public string? Remarks { get; set; } = null;
+        #endregion
+
+        #region Reference Navigation to Recruitment Request
+        [Comment("Foreign key that references primary key: RecruitmentRequest.RequisitionId")]
+        public int RequisitionId { get; set; }
+        public RecruitmentRequest RecruitmentRequest { get; set; } = null!;
+        #endregion
+    }
+}
---
+++ KenHRApp.Domain/Entities/RecruitmentRequest.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KenHRApp.Domain.Entities
+{
+    public class RecruitmentRequest
+    {
+        #region Position Properties
+        public int RequisitionId { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        public string EmploymentTypeCode { get; set; } = null!;
+
+        [NotMapped]
+        public string EmploymentType { get; set; } = null!;
+
+        [Column(TypeName = "varchar(20)")]
+        public string QualificationModeCode { get; set; } = null!;
+
+        [NotMapped]
+        public string QualificationMode { get; set; } = null!;
+
+        [Column(TypeName = "varchar(20)")]
+        public string PositionTypeCode { get; set; } = null!;
+
+        [NotMapped]
+        public string PositionType { get; set; } = null!;
+
+        [Column(TypeName = "varchar(20)")]
+        public string InterviewProcessCode { get; set; } = null!;
+
+        [NotMapped]
+        public string InterviewProcess { get; set; } = null!;
+
+        public bool? IsPreAssessment { get; set; }
+        #endregion
+
+        #region Attributes for Planning Properties
+        [Column(TypeName = "varchar(20)")]
+        public string CompanyCode { get; set; } = null!;
+
+        [NotMapped]
+        public string Company { get; set; } = null!;
+
+        [Column(TypeName = "varchar(20)")]
+        public string DepartmentCode { get; set; } = null!;
+
+        [NotMapped]
+        public string DepartmentName { get; set; } = null!;
+
+        [Column(TypeName = "varchar(20)")]
+        public string? CountryCode { get; set; } = null;
+
+        [NotMapped]
+        [Display(Name = "Country")]
+        public string? Country { get; set; } = null;
+
+        [Column(TypeName = "varchar(20)")]
+        public string? EducationCode { get; set; } = null;
+
+        [NotMapped]
+        public string? Education { get; set; } = null;
+
+        [Column(TypeName = "varchar(20)")]
+        public string EmployeeClassCode { get; set; } = null!;
+
+        [NotMapped]
+        public string EmployeeClass { get; set; } = null!;
+
+        [Column(TypeName = "varchar(20)")]
+        public string? EthnicityCode { get; set; } = null;
+
+        [NotMapped]
+        public string? Ethnicity { get; set; } = null;
+
+        [Column(TypeName = "varchar(20)")]
+        public string JobTitleCode { get; set; } = null!;
+
+        [NotMapped]
+        public string JobTitle { get; set; } = null!;
+
+        [Column(TypeName = "varchar(20)")]
+        public string PayGradeCode { get; set; } = null!;
+
+        [NotMapped]
+        public string PayGradeDesc { get; set; } = null!;
+        #endregion
+
+        #region Position Description Properties        
+        [Column(TypeName = "varchar(1000)")]
+        public string PositionDescription { get; set; } = null!;
+                
+        public int TotalWorkExperience { get; set; }
+        public int? MinWorkExperience { get; set; }
+        public int? MaxWorkExperience { get; set; }
+        public int TotalRelevantExperience { get; set; }
+        public int? MinRelevantExperience { get; set; }
+        public int? MaxRelevantExperience { get; set; }
+        public int AgeRange { get; set; }
+        public int? MinAge { get; set; }
+        public int? MaxAge { get; set; }
+
+        public string? RequiredGender { get; set; } = null;       
+        public string? RequiredAsset { get; set; } = null;
+        public string? VideoDescriptionURL { get; set; } = null;
+        #endregion
+
+        #region Compensation and Benefits
+        public string? SalaryRangeType { get; set; } = null;
+
+        public int? YearlySalaryRange { get; set; }
+        public int? YearlySalaryRangeMin { get; set; }
+        public int? YearlySalaryRangeMax { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        public string? YearlySalaryRangeCurrency { get; set; } = null;
+
+        public int? MonthlySalaryRange { get; set; }
+        public int? MonthlySalaryRangeMin { get; set; }
+        public int? MonthlySalaryRangeMax { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        public string? MonthlySalaryRangeCurrency { get; set; } = null;
+        
+        public int? DailySalaryRange { get; set; }
+        public int? DailySalaryRangeMin { get; set; }
+        public int? DailySalaryRangeMax { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        public string? DailySalaryRangeCurrency { get; set; } = null;
+
+        public int? HourlySalaryRange { get; set; }
+        public int? HourlySalaryRangeMin { get; set; }
+        public int? HourlySalaryRangeMax { get; set; }
+
+        [Column(TypeName = "varchar(20)")]
+        public string? HourlySalaryRangeCurrency { get; set; } = null;
+
+        [Column(TypeName = "varchar(5000)")]
+        public string Responsibilities { get; set; } = null!;
+
+        [Column(TypeName = "varchar(5000)")]
+        public string Competencies { get; set; } = null!;
+
+        [Column(TypeName = "varchar(5000)")]
+        public string? GeneralRemarks { get; set; } = null;
+        #endregion
+
+        #region Reference Navigations
+        public ICollection<JobQualification> QualificationList { get; set; } = new List<JobQualification>();
+        #endregion
+    }
+}
