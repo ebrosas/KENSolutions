c94a9e1ac2d7e402fe4e4512913c0e66dcf3dccc
ebrosas <ervin.brosas@yahoo.com>
2025-11-25 21:30:04 +0300

    Completed DTO classes to be used in Shift Roster page

---
+++ Documentation/Site Maps/Modules/Time and Attendance/Shift Roster/Attendance analysis script.sql
@@ -0,0 +1,20 @@
+
+	--Shift Timing Schedule
+	SELECT * FROM tas.Master_ShiftPatternTitles a
+	WHERE RTRIM(a.ShiftPatCode) = 'AB'
+
+	--Shift Timing Schedule
+	SELECT * FROM tas.Master_ShiftTimes a
+	WHERE RTRIM(a.ShiftPatCode) = 'AB'
+
+	--Shift Pointer
+	SELECT * FROM tas.Master_ShiftPattern a
+	WHERE RTRIM(a.ShiftPatCode) = 'AB'
+
+	SELECT CAST(GETDATE() AS TIME) AS TestTime
+	SELECT CAST(GETDATE() AS TIMESTAMP) AS TestTime
+
+	--Get all shift timings
+	SELECT * FROM tas.syJDE_F0005 a
+	WHERE LTRIM(RTRIM(a.DRSY)) = '06' 
+		AND UPPER(LTRIM(RTRIM(a.DRRT))) = 'SH'
---
+++ KenHRApp.Application/DTOs/ShiftPointerDTO.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KenHRApp.Application.DTOs
+{
+    public class ShiftPointerDTO
+    {
+        #region Properties
+        public int ShiftPointerId { get; set; }
+        public int ShiftPointer { get; set; }
+        public string? Remarks { get; set; } = null;
+        #endregion
+
+        #region Reference Navigation
+        public string ShiftPatternCode { get; set; } = null!;
+        public string ShiftCode { get; set; } = null!;
+        public ShiftPatternMasterDTO ShiftPatternMaster { get; set; } = null!;
+        #endregion
+    }
+}
