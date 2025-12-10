e7200cffa6f769f00b13246c9f105d2db283f5af
ebrosas <ervin.brosas@yahoo.com>
2025-12-10 23:51:42 +0300

    Added AttendanceRepository

---
+++ KenHRApp.Infrastructure/Repositories/AttendanceRepository.cs
@@ -0,0 +1,211 @@
+﻿using KenHRApp.Domain.Entities;
+using KenHRApp.Domain.Models.Common;
+using KenHRApp.Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KenHRApp.Infrastructure.Repositories
+{
+    public class AttendanceRepository : IAttendanceRepository
+    {
+        #region Fields
+        private readonly AppDbContext _db;
+        #endregion
+
+        #region Constructors                
+        public AttendanceRepository(AppDbContext db)
+        {
+            _db = db;
+        }
+        #endregion
+
+        #region Public Methods
+        public async Task<Result<int>> AddShiftRosterMasterAsync(MasterShiftPatternTitle shiftRoster, CancellationToken cancellationToken = default)
+        {
+            int rowsUpdated = 0;
+
+            try
+            {
+                // Save to database
+                _db.MasterShiftPatternTitles.Add(shiftRoster);
+
+                rowsUpdated = await _db.SaveChangesAsync(cancellationToken);
+
+                return Result<int>.SuccessResult(rowsUpdated);
+            }
+            catch (InvalidOperationException invEx)
+            {
+                throw new Exception(invEx.Message.ToString());
+            }
+            catch (Exception ex)
+            {
+                if (ex.InnerException != null)
+                    return Result<int>.Failure($"Database error: {ex.InnerException.Message}");
+                else
+                    return Result<int>.Failure($"Database error: {ex.Message}");
+            }
+        }
+
+        public async Task<Result<int>> UpdateShiftRosterMasterAsync(MasterShiftPatternTitle dto, CancellationToken cancellationToken = default)
+        {
+            int rowsUpdated = 0;
+
+            try
+            {
+                var shiftRoster = await _db.MasterShiftPatternTitles.FirstOrDefaultAsync(x => x.ShiftPatternId == dto.ShiftPatternId, cancellationToken);
+                if (shiftRoster == null)
+                    throw new InvalidOperationException("Shift Roster record not found");
+
+                #region Update Shift Roster 
+                shiftRoster.ShiftPatternCode = dto.ShiftPatternCode;
+                shiftRoster.ShiftPatternDescription = dto.ShiftPatternDescription;
+                shiftRoster.IsActive = dto.IsActive;
+                shiftRoster.IsDayShift = dto.IsDayShift;
+                shiftRoster.IsFlexiTime = dto.IsFlexiTime;
+                shiftRoster.LastUpdateEmpNo = dto.LastUpdateEmpNo;
+                shiftRoster.LastUpdateUserID = dto.LastUpdateUserID;
+                shiftRoster.LastUpdatedByName = dto.LastUpdatedByName;
+                shiftRoster.LastUpdateDate = dto.LastUpdateDate;
+                #endregion
+
+                #region Update Shift Timing
+                if (dto.ShiftTimingList != null && dto.ShiftTimingList.Any())
+                {
+                    #region Delete entity items that don't exist in the DTO
+                    var shiftTimingNotInDTO = _db.MasterShiftTimes.AsEnumerable()
+                                    .ExceptBy(dto.ShiftTimingList.Select(d => d.ShiftTimingId), e => e.ShiftTimingId)
+                                    .ToList();
+                    if (shiftTimingNotInDTO.Any())
+                    {
+                        _db.MasterShiftTimes.RemoveRange(shiftTimingNotInDTO);
+                        await _db.SaveChangesAsync();
+                    }
+                    #endregion
+
+                    foreach (var shiftTiming in dto.ShiftTimingList)
+                    {
+                        var existingShiftTiming = await _db.MasterShiftTimes
+                            .FirstOrDefaultAsync(st => st.ShiftPatternCode == dto.ShiftPatternCode && st.ShiftCode == shiftTiming.ShiftCode, cancellationToken);
+
+                        if (existingShiftTiming != null)
+                        {
+                            // Update existing shiftTiming
+                            existingShiftTiming.ShiftDescription = shiftTiming.ShiftDescription;
+                            existingShiftTiming.ArrivalFrom = shiftTiming.ArrivalFrom;
+                            existingShiftTiming.ArrivalTo = shiftTiming.ArrivalTo;
+                            existingShiftTiming.DepartFrom = shiftTiming.DepartFrom;
+                            existingShiftTiming.DepartTo = shiftTiming.DepartTo;
+                            existingShiftTiming.DurationNormal = Convert.ToInt32((shiftTiming.ArrivalTo - shiftTiming.DepartFrom).Duration().TotalMinutes);
+                            existingShiftTiming.RArrivalFrom = shiftTiming.RArrivalFrom;
+                            existingShiftTiming.RArrivalTo = shiftTiming.RArrivalTo;
+                            existingShiftTiming.RDepartFrom = shiftTiming.RDepartFrom;
+                            existingShiftTiming.RDepartTo = shiftTiming.RDepartTo;
+
+                            if (shiftTiming.RArrivalTo.HasValue && shiftTiming.RDepartFrom.HasValue)
+                                existingShiftTiming.DurationRamadan = Convert.ToInt32((shiftTiming.RArrivalTo.Value - shiftTiming.RDepartFrom.Value).Duration().TotalMinutes);
+
+                            existingShiftTiming.LastUpdateDate = shiftTiming.LastUpdateDate;
+                            existingShiftTiming.LastUpdateEmpNo = shiftTiming.LastUpdateEmpNo;
+                            existingShiftTiming.LastUpdatedByName = shiftTiming.LastUpdatedByName;
+                            existingShiftTiming.LastUpdateUserID = shiftTiming.LastUpdateUserID;
+                        }
+                        else
+                        {
+                            // Add new Shift Timing
+                            var newShiftTiming = new MasterShiftTime
+                            {
+                                ShiftPatternCode = shiftRoster.ShiftPatternCode,
+                                ShiftCode = shiftTiming.ShiftCode,
+                                ShiftDescription = shiftTiming.ShiftDescription,
+                                ArrivalFrom = shiftTiming.ArrivalFrom,
+                                ArrivalTo = shiftTiming.ArrivalTo,
+                                DepartFrom = shiftTiming.DepartFrom,
+                                DepartTo = shiftTiming.DepartTo,
+                                DurationNormal = Convert.ToInt32((shiftTiming.ArrivalTo - shiftTiming.DepartFrom).Duration().TotalMinutes),
+                                RArrivalFrom = shiftTiming.RArrivalFrom,
+                                RArrivalTo = shiftTiming.RArrivalTo,
+                                RDepartFrom = shiftTiming.RDepartFrom,
+                                RDepartTo = shiftTiming.RDepartTo,
+                                CreatedDate = shiftTiming.CreatedDate,
+                                CreatedByEmpNo = shiftTiming.CreatedByEmpNo,
+                                CreatedByName = shiftTiming.CreatedByName,
+                                CreatedByUserID = shiftTiming.CreatedByUserID
+                            };
+
+                            if (shiftTiming.RArrivalTo.HasValue && shiftTiming.RDepartFrom.HasValue)
+                                newShiftTiming.DurationRamadan = Convert.ToInt32((shiftTiming.RArrivalTo.Value - shiftTiming.RDepartFrom.Value).Duration().TotalMinutes);
+
+                            await _db.MasterShiftTimes.AddAsync(newShiftTiming, cancellationToken);
+                        }
+                    }
+                }
+                #endregion
+
+                #region Update Shift Pointer
+                if (dto.ShiftPointerList != null && dto.ShiftPointerList.Any())
+                {
+                    #region Delete entity items that don't exist in the DTO
+                    var shiftPointerNotInDTO = _db.MasterShiftPatterns.AsEnumerable()
+                                    .ExceptBy(dto.ShiftPointerList.Select(d => d.ShiftPointerId), e => e.ShiftPointerId)
+                                    .ToList();
+                    if (shiftPointerNotInDTO.Any())
+                    {
+                        _db.MasterShiftPatterns.RemoveRange(shiftPointerNotInDTO);
+                        await _db.SaveChangesAsync();
+                    }
+                    #endregion
+
+                    foreach (var shiftPointer in dto.ShiftPointerList)
+                    {
+                        var existingShiftPointer = await _db.MasterShiftPatterns
+                            .FirstOrDefaultAsync(st => st.ShiftPatternCode == dto.ShiftPatternCode 
+                                && st.ShiftCode == shiftPointer.ShiftCode
+                                && st.ShiftPointer == shiftPointer.ShiftPointer, cancellationToken);
+
+                        if (existingShiftPointer != null)
+                        {
+                            // Update existing Shift Pointer
+                            existingShiftPointer.ShiftDescription = shiftPointer.ShiftDescription;
+                            existingShiftPointer.ShiftPointer = shiftPointer.ShiftPointer;
+                        }
+                        else
+                        {
+                            // Add new Shift Pointer
+                            var newShiftPointer = new MasterShiftPattern
+                            {
+                                ShiftPatternCode = shiftRoster.ShiftPatternCode,
+                                ShiftCode = shiftPointer.ShiftCode,
+                                ShiftDescription = shiftPointer.ShiftDescription,
+                                ShiftPointer = shiftPointer.ShiftPointer
+                            };
+
+                            await _db.MasterShiftPatterns.AddAsync(shiftPointer, cancellationToken);
+                        }
+                    }
+                }
+                #endregion
+
+                // Save to database
+                _db.MasterShiftPatternTitles.Update(shiftRoster);
+
+                rowsUpdated = await _db.SaveChangesAsync(cancellationToken);
+
+                return Result<int>.SuccessResult(rowsUpdated);
+            }
+            catch (InvalidOperationException invEx)
+            {
+                throw new Exception(invEx.Message.ToString());
+            }
+            catch (Exception ex)
+            {
+                return Result<int>.Failure($"Database error: {ex.Message}");
+            }
+        }
+        #endregion
+    }
+}
---
+++ KenHRApp.Infrastructure/Repositories/IAttendanceRepository.cs
@@ -0,0 +1,18 @@
+﻿using KenHRApp.Domain.Entities;
+using KenHRApp.Domain.Models.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KenHRApp.Infrastructure.Repositories
+{
+    public interface IAttendanceRepository
+    {
+        #region Public Methods
+        Task<Result<int>> AddShiftRosterMasterAsync(MasterShiftPatternTitle shiftRoster, CancellationToken cancellationToken = default);
+        Task<Result<int>> UpdateShiftRosterMasterAsync(MasterShiftPatternTitle dto, CancellationToken cancellationToken = default);
+        #endregion
+    }
+}
