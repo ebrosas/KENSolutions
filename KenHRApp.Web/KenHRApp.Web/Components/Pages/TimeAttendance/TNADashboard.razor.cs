39594ceac6fe6b2f0f12c6d6859e44bec4f2c2fa
ebrosas <ervin.brosas@yahoo.com>
2026-01-18 16:54:35 +0300

    Started designing the Attendance Dashboard page

---
+++ KenHRApp.Web/KenHRApp.Web/Components/Pages/TimeAttendance/TNADashboard.razor.cs
@@ -0,0 +1,137 @@
+﻿using KenHRApp.Application.Common.Interfaces;
+using KenHRApp.Application.DTOs;
+using KenHRApp.Application.Interfaces;
+using KenHRApp.Application.Services;
+using KenHRApp.Domain.Entities;
+using KenHRApp.Web.Components.Shared;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using MudBlazor;
+using System.Globalization;
+using System.Text;
+
+namespace KenHRApp.Web.Components.Pages.TimeAttendance
+{
+    public partial class TNADashboard
+    {
+        #region Parameters and Injections
+        [Inject] private IAttendanceService AttendanceService { get; set; } = default!;
+        [Inject] private IDialogService DialogService { get; set; } = default!;
+        [Inject] private ISnackbar Snackbar { get; set; } = default!;
+        [Inject] private ILookupCacheService LookupCache { get; set; } = default!;
+        [Inject] private NavigationManager Navigation { get; set; } = default!;
+        [Inject] private IAppState AppState { get; set; } = default!;
+        #endregion
+
+        #region Fields
+
+        #region Private Fields
+        private string overlayMessage = "Please wait...";
+        private CancellationTokenSource? _cts;
+        private string _searchString = string.Empty;
+        private StringBuilder _errorMessage = new StringBuilder();
+        private List<string> _events = new();
+        #endregion
+
+        #region Flags
+        private bool _showErrorAlert = false;
+        private bool _hasValidationError = false;
+        private bool _isRunning = false;
+        private bool _enableFilter = false;
+        private bool _isTaskFinished = false;
+        #endregion
+
+        #region Dialog Box Button Icons
+        private readonly string _iconDelete = "fas fa-trash-alt";
+        private readonly string _iconCancel = "fas fa-window-close";
+        private readonly string _iconError = "fas fa-times-circle";
+        private readonly string _iconInfo = "fas fa-info-circle";
+        private readonly string _iconWarning = "fas fa-exclamation-circle";
+        #endregion
+
+        #region Collections        
+        private List<ShiftPatternMasterDTO> _shiftRosterList = new List<ShiftPatternMasterDTO>();
+        private List<BreadcrumbItem> _breadcrumbItems =
+        [
+            new("Home", href: "/", icon: Icons.Material.Filled.Home),
+            new("Employee Attendance Dashboard", href: null, disabled: true, @Icons.Material.Filled.CalendarMonth)
+        ];
+        #endregion
+
+        #endregion
+
+        #region Enums
+        private enum SnackBarTypes
+        {
+            Normal,
+            Information,
+            Success,
+            Warning,
+            Error
+        }
+        #endregion
+
+        #region Page Events
+        protected override void OnInitialized()
+        {
+            //BeginSearchShiftRoster();
+        }
+        #endregion
+
+        #region Private Methods
+        private void ShowNotification(string message, SnackBarTypes type)
+        {
+            Snackbar.Clear();
+
+            Snackbar.Configuration.PositionClass = Defaults.Classes.Position.TopCenter;
+            Snackbar.Configuration.PreventDuplicates = false;
+            Snackbar.Configuration.NewestOnTop = false;
+            Snackbar.Configuration.ShowCloseIcon = true;
+            Snackbar.Configuration.VisibleStateDuration = 5000;
+            Snackbar.Configuration.HideTransitionDuration = 500;
+            Snackbar.Configuration.ShowTransitionDuration = 500;
+            Snackbar.Configuration.SnackbarVariant = Variant.Filled;
+
+            switch (type)
+            {
+                case SnackBarTypes.Information:
+                    Snackbar.Add(message, Severity.Info);
+                    break;
+
+                case SnackBarTypes.Success:
+                    Snackbar.Add(message, Severity.Success);
+                    break;
+
+                case SnackBarTypes.Warning:
+                    Snackbar.Add(message, Severity.Warning);
+                    break;
+
+                case SnackBarTypes.Error:
+                    Snackbar.Add(message, Severity.Error);
+                    break;
+
+                default:
+                    Snackbar.Add(message, Severity.Normal);
+                    break;
+            }
+
+            // Snackbar.Add($"Error {message}", Severity.Error, c => c.SnackbarVariant = Variant.Filled);
+        }
+
+        private void ShowHideError(bool value)
+        {
+            if (value)
+            {
+                _showErrorAlert = true;
+            }
+            else
+            {
+                _showErrorAlert = false;
+
+                // Reset error messages
+                _errorMessage.Clear();
+            }
+        }
+        #endregion
+    }
+}
