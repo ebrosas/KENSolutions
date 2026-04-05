77524bba6ad69a1c84fb6a7af226bea65fc56405
ebrosas <ervin.brosas@yahoo.com>
2026-04-05 21:46:25 +0300

    Enhanced the design of the leave entitlement pop-up form

--- DBScripts/CreateScripts/Data/Data script.sql
+++ DBScripts/CreateScripts/Data/Data script.sql
@@ -33,7 +33,7 @@
 	--Attendance Legends
 	SELECT a.* 
 	FROM kenuser.UserDefinedCode a WITH (NOLOCK)
-	WHERE a.GroupID = (SELECT x.UDCGroupId FROM kenuser.UserDefinedCodeGroup x WITH (NOLOCK) WHERE RTRIM(x.UDCGCode) = 'ATTENDLEGEND')	
+	WHERE a.GroupID = (SELECT x.UDCGroupId FROM kenuser.UserDefinedCodeGroup x WITH (NOLOCK) WHERE RTRIM(x.UDCGCode) = 'RENEWTYPE')	
 
 	SELECT a.* 
 	FROM kenuser.UserDefinedCode a WITH (NOLOCK)
